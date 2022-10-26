2392e27ce60381e944adae0690f40ce08cac455c
kasperaj <andrcasper@gmail.com>
2022-10-26 19:29:49 +0300

    Пытаюсь сделать комит, попытка 8

--- homework01/rsa.py
+++ homework01/rsa.py
@@ -17,7 +17,7 @@ def is_prime(n: int) -> bool:
         return True
     if n % 2 == 0 or n < 2:
         return False
-    for i in range(3, int(n ** 0.5) + 1, 2):
+    for i in range(3, int(n**0.5) + 1, 2):
         if n % i == 0:
             return False
     return True
@@ -95,7 +95,7 @@ def encrypt(pk: tp.Tuple[int, int], plaintext: str) -> tp.List[int]:
     key, n = pk
     # Convert each letter in the plaintext to numbers based on
     # the character using a^b mod m
-    cipher = [(ord(char) ** key) % n for char in plaintext]
+    cipher = [(ord(char)**key) % n for char in plaintext]
     # Return the array of bytes
     return cipher
 
@@ -104,7 +104,7 @@ def decrypt(pk: tp.Tuple[int, int], ciphertext: tp.List[int]) -> str:
     # Unpack the key into its components
     key, n = pk
     # Generate the plaintext based on the ciphertext and key using a^b mod m
-    plain = [chr((char ** key) % n) for char in ciphertext]
+    plain = [chr((char**key) % n) for char in ciphertext]
     # Return the array of bytes as a string
     return "".join(plain)
 
