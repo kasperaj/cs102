f27baa43ba543436c44689256ce597a75b783a29
kasperaj <69190749+kasperaj@users.noreply.github.com>
2022-12-07 05:00:07 +0300

    Update life.py

--- homework03/life.py
+++ homework03/life.py
@@ -54,7 +54,8 @@ class GameOfLife:
         y, x = cell
         for i in [-1, 0, 1]:
             for j in [-1, 0, 1]:
-                if (x + i != x or y + j != y) and (0 <= x + i <= self.cols - 1) and (0 <= y + j <= self.rows - 1)):
+                if (x + i != x or y + j != y) and (0 <= x + i <= self.cols - 1) and (
+                        0 <= y + j <= self.rows - 1):
                     neighbour_x = x + i
                     neighbour_y = y + j
                     self.neighbours.append(self.curr_generation[neighbour_y][neighbour_x])
